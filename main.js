f1ec562b8d661ac299fe8c0235a29202d322ca38
Ryan Alli <ryanalli77@gmail.com>
2024-01-02 12:55:23 -0500

    javascript functionality for operations

--- main.js
+++ main.js
@@ -1,4 +1,79 @@
-btn = document.getElementById("hi").onclick = function()
+let btn = Array.from(document.querySelectorAll(".btn"));
+let display = document.querySelector(".num-box");
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+for(let i = 0; i < btn.length; i++)
 {
-  console.log('hi')
-};
+  btn[i].addEventListener('click', function()
+  {
+    if(display.innerHTML === "00")
+    {
+      display.innerHTML = '';
+    }
+    txt = btn[i].textContent
+    if(txt === "clear")
+    {
+      num1 = '';
+      num2 = '';
+      operator = '';
+      display.innerHTML = '00'
+    }
+    else if(txt === "/" || txt === "x" || txt === "-" || txt === "+")
+    {
+      operator = txt;
+      display.innerHTML += txt;
+    }
+    else if(txt === "=")
+    {
+      operate(num1, num2, operator);
+    }
+    else if(operator === "")
+    {
+      num1 += txt;
+      display.innerHTML += txt;
+    }
+    else
+    {
+      num2 += txt
+      display.innerHTML += txt;
+    }
+  });
+}
+
+function operate(n1, n2, op)
+{
+  let result;
+  if(op === '/')
+  {
+    display.innerHTML = Number(n1) / Number(n2);
+    result = Number(n1) / Number(n2);
+    num1 = result.toString();
+  }
+  else if(op === 'x')
+  {
+    display.innerHTML = Number(n1) * Number(n2);
+    result = Number(n1) * Number(n2);
+    num1 = result.toString();
+    num1 = result.toString();
+  }
+  else if(op === '-')
+  {
+    display.innerHTML = Number(n1) - Number(n2);
+    result = Number(n1) - Number(n2);
+    num1 = result.toString();
+    num1 = result.toString();
+  }
+  else if(op === '+')
+  {
+    display.innerHTML = Number(n1) + Number(n2);
+    result = Number(n1) + Number(n2);
+    num1 = result.toString();
+  }
+  num2 = '';
+  console.log(n1);
+  console.log(op);
+  console.log(n2);
+
+}
